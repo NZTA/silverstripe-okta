30201b12f5e00d7ba0b2b98402a2ba6a99da51bf
Ishan Jayamanne <ishanjmails@gmail.com>
2018-10-31 03:26:53 +0000

    Remove hardcoded URL

--- src/OktaFactory.php
+++ src/OktaFactory.php
@@ -6,13 +6,14 @@ use OneLogin\Saml2\Utils;
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Environment;
 use SilverStripe\Core\Injector\Factory;
-
+use SilverStripe\Control\Controller;
 
 class OktaFactory implements Factory
 {
     /**
      * @param string $service
      * @param array $params
+     *
      * @return \NZTA\Okta\Okta|object
      * @throws \OneLogin_Saml2_Error
      */
@@ -41,54 +42,51 @@ class OktaFactory implements Factory
         $oktaStrict = Environment::getEnv('SS_OKTA_STRICT');
 
         $config = [
-            'strict' => !Director::isDev(),
-            'debug' => Director::isDev(),
-            'sp' => [
-                'entityId' => Environment::getEnv('SS_OKTA_SP_ISSUER'),
-                'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:entity',
-                'x509cert' => !empty($SPX509cert) ? Utils::formatCert(file_get_contents($SPX509cert)) : '',
-                'privateKey' => !empty($SPPem) ? Utils::formatPrivateKey(file_get_contents($SPPem)) : '',
+            'strict' => ($oktaStrict === false) ? false : true,
+            'debug'  => Director::isDev(),
+            'sp'     => [
+                'entityId'                 => Environment::getEnv('SS_OKTA_SP_ISSUER'),
+                'NameIDFormat'             => 'urn:oasis:names:tc:SAML:2.0:nameid-format:entity',
+                'x509cert'                 => !empty($SPX509cert) ? Utils::formatCert(file_get_contents($SPX509cert)) : '',
+                'privateKey'               => !empty($SPPem) ? Utils::formatPrivateKey(file_get_contents($SPPem)) : '',
                 'assertionConsumerService' => [
-                    'url' => 'http://nztaintranet.local/okta/sso',//Controller::join_links(Director::absoluteBaseURL(), 'okta', 'sso'),
+                    'url'     => Controller::join_links(Director::absoluteBaseURL(), 'okta', 'sso'),
                     'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
                 ],
-                'singleLogoutService' => [
-                    'url' => Environment::getEnv('SS_OKTA_SP_LOGOUT_URL'),
+                'singleLogoutService'      => [
+                    'url'     => Environment::getEnv('SS_OKTA_SP_LOGOUT_URL'),
                     'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-                ]
+                ],
             ],
 
             'idp' => [
-                'entityId' => Environment::getEnv('SS_OKTA_IDP_ISSUER'),
-                'x509cert' => !empty($IDPX509cert) ? Utils::formatCert(file_get_contents($IDPX509cert)) : '',
+                'entityId'            => Environment::getEnv('SS_OKTA_IDP_ISSUER'),
+                'x509cert'            => !empty($IDPX509cert) ? Utils::formatCert(file_get_contents($IDPX509cert)) : '',
                 'singleSignOnService' => [
-                    'url' => $IDPLoginURL,
+                    'url'     => $IDPLoginURL,
                     'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
                 ],
                 'singleLogoutService' => [
-                    'url' => $IDPLogoutURL,
+                    'url'     => $IDPLogoutURL,
                     'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-                ]
+                ],
             ],
 
             'security' => [
-                'authnRequestsSigned' => true,
-                'logoutRequestSigned' => true,
+                'authnRequestsSigned'  => true,
+                'logoutRequestSigned'  => true,
                 'logoutResponseSigned' => true,
-                'wantMessagesSigned' => true,
-                'signatureAlgorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
-            ]
+                'wantMessagesSigned'   => true,
+                'signatureAlgorithm'   => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
+            ],
         ];
-
-        if (!empty($oktaStrict)) {
-            $config['strict'] = (bool)$oktaStrict;
-        }
 
         return $config;
     }
 
     /**
      * Ensures all required constants have been defined. if not throw an exception.
+     *
      * @throws \Exception
      */
     private function ensureDefinedConstants()
